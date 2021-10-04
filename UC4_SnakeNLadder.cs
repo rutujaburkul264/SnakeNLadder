de2ddc72f94d31f7e0f48bc82eb732a1c0e0e833
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-04 20:10:28 +0530

    Player

---
+++ UC4_SnakeNLadder.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+
+namespace SnakeNLadder
+{
+    class Program
+    {
+        public static int Main(String[] args)
+        {
+	  UC2_SnakeNLadder player = new UC2_SnakeNLadder();
+          UC3_SnakeNLadder board = new UC3_SnakeNLadder();
+	  
+	  Console.WriteLine("\n*****************************************************************\n");
+          Console.WriteLine("\t\t Snake and Ladder Board");
+          Console.WriteLine("\n*****************************************************************\n");
+	 
+	  int score;
+            int count = 0;
+            while (true)//UC4-repeat till the player reaches winning position 100
+            {
+                player.previousPosition = player.currentPosition;
+                score = player.RollingDice();
+                count1++;
+                player.currentPosition = board.nextposition(score, player.previousPosition);
+                if (player.currentPosition > 100)
+                    player.currentPosition = player.previousPosition;
+
+		if (player.currentPosition == 100) //Ensure the player gets winning position
+                {
+                    Console.WriteLine("Player won the Game");
+                    break;
+                }
+            }
+	
+ 	Thread.Sleep(2000);
+        return 1;
+	}
+}
