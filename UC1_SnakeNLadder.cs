708c46b45ced175cf59ab087067b5c81c3e2f58d
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-04 19:55:17 +0530

    played with single player

---
+++ UC1_SnakeNLadder.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeNLadder
+{
+    class UC1_SnakeNLadder
+    {
+        public string name;
+        public int currentPosition;
+        public int previousPosition;
+
+        public void player()
+        {
+            name = "";
+            currentPosition = 0;
+            previousPosition = 0;
+        }
+    }
+}
