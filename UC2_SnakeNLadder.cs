1132493eb45e151b5189e51c2162715a28a5910e
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-04 19:58:07 +0530

    Die Roll 1 to 6

---
+++ UC2_SnakeNLadder.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeNLadder
+{
+    class UC2_SnakeNLadder
+    {
+        public string name;
+        public int currentPosition;
+        public int previousPosition;
+
+        public void player()
+        {
+            name = "";
+            currentPosition = 0;
+            previousPosition = 0;
+        }
+        public int RollingDice()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
+        }
+    }
+
+}
